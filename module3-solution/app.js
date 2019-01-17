2aa21bd6dc3957d940028070df46bac1995ecaba
Nihal <Nihal@wizitsolutions.in>
2019-01-17 23:28:32 +0530

    assignment 3

---
+++ module3-solution/app.js
@@ -0,0 +1,54 @@
+(function () {
+  angular.module("menuApp",[])
+  .controller("myController",myControllerMain)
+  .service("fetchService",fetchServiceMain)
+  .directive("menuItem",menuItems);
+  function menuItems() {
+    var ddo={
+      templateUrl:"items.html"
+    }
+    return ddo;
+  }
+  myControllerMain.$inject=['fetchService'];
+  function myControllerMain(fetchService) {
+    var x;
+    x= this;
+    x.inp="";
+    x.fetch = function() {
+      x.show=[];
+      var promise = fetchService.getItems();
+      promise.then(function (response) {
+        x.data = response.data.menu_items;
+
+        for(var it=0;it<x.data.length;it++)
+            {
+              var name =x.data[it].name;
+              if(name.indexOf(x.inp) !=-1)
+              {
+                x.show.push(x.data[it]);
+              }
+            }
+          })
+          .catch(function(error) {
+        console.log("ni ho paega");
+      });
+
+    };
+  x.remove=function (index) {
+    x.show.splice(index,1);
+  }
+  };
+  fetchServiceMain.$inject=['$http']
+  function fetchServiceMain($http) {
+    var fetch=this;
+    fetch.getItems=function () {
+      var result=$http({
+        mehtod:"GET",
+        url:("https://davids-restaurant.herokuapp.com/menu_items.json")
+      });
+      console.log(result);
+      return result;
+    };
+
+  };
+})();
