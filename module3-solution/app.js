054bc4bcff8e792ce4967a893603d99ed7751b66
Nihal <Nihal@wizitsolutions.in>
2019-01-23 19:18:32 +0530

    Fixed Bugs

--- module3-solution/app.js
+++ module3-solution/app.js
@@ -15,25 +15,41 @@
     x= this;
     x.inp="";
     x.fetch = function() {
+      if(x.inp!==""){
       x.show=[];
       var promise = fetchService.getItems();
       promise.then(function (response) {
+        x.error="";
         x.data = response.data.menu_items;
+        console.log(x.inp);
 
-        for(var it=0;it<x.data.length;it++)
-            {
-              var name =x.data[it].name;
-              if(name.indexOf(x.inp) !=-1)
+          console.log("in");
+          console.log(x.data);
+          for(var it=0;it<x.data.length;it++)
               {
-                x.show.push(x.data[it]);
+                var name =x.data[it].description;
+                if(name.indexOf(x.inp) !=-1)
+                {
+                  x.show.push(x.data[it]);
+                }
               }
-            }
+          console.log(x.show.length);
+          if(x.show.length===0)
+          {
+            x.error="Nothing found";
+            
+          }
           })
-          .catch(function(error) {
-        console.log("Some Thing Went Worng!");
-      });
+            .catch(function(error) {
+          console.log("Some Thing Went Worng!");
+        });
 
-    };
+    }
+    else {
+        x.error="Nothing found";
+    }
+  };
+
   x.remove=function (index) {
     x.show.splice(index,1);
   }
